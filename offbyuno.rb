f0e47417d311e585748a83320fd5b5968afff4c8
Chloe Krainbucher <chloekrainbucher@chloes-mbp.home>
2017-02-12 19:13:07 -0500

    many numbers work

--- offbyuno.rb
+++ offbyuno.rb
@@ -3,15 +3,17 @@ def off_by_uno(num, win)
 check = 0
 index_pos = 0
 
-	if num[index_pos] = win[index_pos]
-		check += 1
-	else
-		check += 0
+	num.length.times do
+
+		if num[index_pos] == win[index_pos]
+			check += 1
+		end
+		index_pos += 1
 	end
 
-	if check = 3
-		true
-	else
-		false
-	end
+		if num.length - check == 1
+			true
+		else
+			false
+		end
 end 
--- test_offbyuno.rb
+++ test_offbyuno.rb
@@ -1,10 +1,22 @@
 require 'minitest/autorun'
-require_relative 'offby1_redo.rb'
+require_relative 'offbyuno.rb'
 
 class TestOff < MiniTest::Test 
 	def test_off_by1_true
 		num = '1234'
 		win = '2234'
+		assert_equal(true, off_by_uno(num, win))
+	end
+
+	def test_equal_false
+		num = '1234'
+		win = '1234'
+		assert_equal(false, off_by_uno(num, win))
+	end
+
+	def test_many_num_true
+		num = '123456'
+		win = '123455'
 		assert_equal(true, off_by_uno(num, win))
 	end
 end 
